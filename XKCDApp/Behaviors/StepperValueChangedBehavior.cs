3a096eb7048d162b75e129b0672d49017a83d730
Vincent Nyanga <vince.nyanga@hotmail.com>
2020-09-09 12:34:27 +0200

    added behavior

---
+++ XKCDApp/Behaviors/StepperValueChangedBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XKCDApp.Behaviors
+{
+    public class StepperValueChangedBehavior : Behavior<Stepper>
+    {
+        public ICommand Command { get; set; }
+
+        protected override void OnAttachedTo(Stepper stepper)
+        {
+            stepper.ValueChanged += OnValueChanged;
+            base.OnAttachedTo(stepper);
+        }
+
+        protected override void OnDetachingFrom(Stepper stepper)
+        {
+            stepper.ValueChanged -= OnValueChanged;
+            base.OnDetachingFrom(stepper);
+        }
+
+        private void OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (Command.CanExecute(e.NewValue))
+            {
+                Command.Execute(e.NewValue);
+            }
+        }
+    }
+}
+    
