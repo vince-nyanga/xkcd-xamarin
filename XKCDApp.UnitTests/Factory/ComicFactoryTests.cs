c021513fcc5c562f244876f7420786c62f870729
Vincent Nyanga <vince.nyanga@hotmail.com>
2020-09-09 07:54:57 +0200

    models

---
+++ XKCDApp.UnitTests/Factory/ComicFactoryTests.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using XKCDApp.Factory;
+using Xunit;
+
+namespace XKCDApp.UnitTests.Factory
+{
+    public class ComicFactoryTests
+    {
+        [Fact]
+        public void FromJson_WithValidString_ShouldReturnComic()
+        {
+            var jsonString = GetValidString();
+
+            var comic = ComicFactory.FromJson(jsonString);
+
+            Assert.NotNull(comic);
+            Assert.Equal(2020, comic.Date.Year);
+        }
+
+        [Fact]
+        public void FromJson_WithInvalidString_ShouldNotReturnComic()
+        {
+            var jsonString = "invalid";
+
+            var comic = ComicFactory.FromJson(jsonString);
+
+            Assert.Null(comic);
+        }
+
+        private string GetValidString()
+        {
+            var jobject = new JObject();
+            jobject.Add("year", "2020");
+            jobject.Add("month", "9");
+            jobject.Add("day", "9");
+            jobject.Add("safe_title", "Test");
+            jobject.Add("img", "Test");
+            jobject.Add("alt", "Test");
+            jobject.Add("transcript", "Test");
+            return jobject.ToString();
+        }
+    }
+}
---
+++ XKCDApp/Models/Comic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace XKCDApp.Models
+{
+    public class Comic
+    {
+        public DateTime Date { get; set; }
+
+        [JsonProperty("safe_title")]
+        public string SafeTitle { get; set; }
+
+        [JsonProperty("transcript")]
+        public string Transcript { get; set; }
+
+        [JsonProperty("alt")]
+        public string AlternateText { get; set; }
+
+        [JsonProperty("img")]
+        public string ImageUrl { get; set; }
+    }
+}
